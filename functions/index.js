bdb6c3933a3009d4f715ab6e13f9a0b8ccc4de2f
siddheshkothadi <realsiddheshko@gmail.com>
2022-12-11 19:30:28 +0530

    change function

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,33 @@
+const sampleResponse = {
+  "session": {
+    "id": "session_id",
+    "params": {
+      "key": "value"
+    }
+  },
+  "prompt": {
+    "override": false,
+    "firstSimple": {
+      "speech": "Hello world.",
+      "text": "Hello world."
+    }
+  },
+  "user": {
+    "locale": "en-US",
+    "params": {
+      "verificationStatus": "VERIFIED",
+      "key": "value"
+    }
+  },
+  "home": {
+    "params": {
+      "key": "value"
+    }
+  }
+}
+
+
 export default (req, res) => {
-  res.status(200).send(`Hello from JavaScript`)
+  console.log(req);
+  res.status(200).json(sampleResponse);
 }
